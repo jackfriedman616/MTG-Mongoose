e687bce27923922699d03e1ec8fd43d17f5351c8
jackfriedman616 <124191379+jackfriedman616@users.noreply.github.com>
2023-12-09 14:06:04 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-mongoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(secret.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
